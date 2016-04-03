a71bcaa398444d1ccde75bdc8a7b4166d0a4c9b6
Ocki Bagus Pratama <ocki.bagus.p@gmail.com>
2016-04-03 17:31:43 +0700

    Prepare for Django 1.10

--- people/views.py
+++ people/views.py
@@ -1,25 +1,27 @@
-from django.shortcuts import render
-from  django.views.generic.list import ListView
-from  people.models import Category, Person
-from  django.views.generic.edit import CreateView, UpdateView, DeleteView
-from  people.forms import CategoryForm, PersonForm
-from  django.views.generic.detail import DetailView
-from  django.shortcuts import get_object_or_404
-from  django.core.urlresolvers import reverse
+from django.views.generic.list import ListView
+from people.models import Category, Person
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from people.forms import CategoryForm, PersonForm
+from django.views.generic.detail import DetailView
+from django.shortcuts import get_object_or_404
+from django.core.urlresolvers import reverse
 
 
 class CategoryMixin(object):
     model = Category
-    def get_context_data(self, **kwargs):
-        kwargs.update({'object_name':'Category'})
+
+    @staticmethod
+    def get_context_data(**kwargs):
+        kwargs.update({'object_name': 'Category'})
         return kwargs
-    
+
+
 class CategoryFormMixin(CategoryMixin):
     form_class = CategoryForm
     template_name = 'people/object_form.html'
 
 
-class CategoryList (CategoryMixin, ListView):
+class CategoryList(CategoryMixin, ListView):
     template_name = 'people/object_list.html'
 
 
@@ -35,16 +37,19 @@ class EditCategory(CategoryFormMixin, UpdateView):
     pass
 
 
-class DeleteCategory (CategoryMixin, DeleteView):
+class DeleteCategory(CategoryMixin, DeleteView):
     template_name = 'people/object_confirm_delete.html'
+
     def get_success_url(self):
         return reverse('category_list')
-    
-    
+
+
 class PersonMixin(object):
     model = Person
-    def get_context_data(self, **kwargs):
-        kwargs.update({'object_name':'Person'})
+
+    @staticmethod
+    def get_context_data(**kwargs):
+        kwargs.update({'object_name': 'Person'})
         return kwargs
 
 
@@ -63,20 +68,25 @@ class ViewPerson(PersonMixin, DetailView):
 
 class NewPerson(PersonFormMixin, CreateView):
     pass
- 
- 
+
+
 class EditPerson(PersonFormMixin, UpdateView):
     pass
 
 
 class KillPerson(PersonMixin, DeleteView):
     template_name = 'people/object_confirm_delete.html'
+
     def get_success_url(self):
         return reverse('people_list')
 
 
-class ViewCategory (PersonMixin, ListView):
+class ViewCategory(PersonMixin, ListView):
     template_name = 'people/object_list.html'
-    def get_queryset(self):
+
+    def __init__(self, **kwargs):
+        super(ViewCategory, self).__init__(**kwargs)
         self.category = get_object_or_404(Category, slug=self.kwargs['slug'])
+
+    def get_queryset(self):
         return super(ViewCategory, self).get_queryset().filter(category=self.category)
--- servers/urls.py
+++ servers/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls import patterns, url
 
 from servers import views
 
-urlpatterns = patterns('',
+urlpatterns = [
   url(r'^$', views.server_list, name='server_list'),
   url(r'^new$', views.server_create, name='server_new'),
   url(r'^edit/(?P<pk>\d+)$', views.server_update, name='server_edit'),
   url(r'^delete/(?P<pk>\d+)$', views.server_delete, name='server_delete'),
-)
+]
